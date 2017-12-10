37b8ad9a4a3f5b48bdceb0827c6f013c5ac0f69f
nitram509 <maki@bitkings.de>
2017-12-10 01:13:05 +0100

    refactoring noise and led animation into separate methods

--- doodle-ultraschallsensor-hc-sr04/doodle-ultraschallsensor-hc-sr04.ino
+++ doodle-ultraschallsensor-hc-sr04/doodle-ultraschallsensor-hc-sr04.ino
@@ -1,15 +1,17 @@
 #include <FastLED.h>
 
-#define TRIGGER 3 // Arduino Pin --> HC-SR04 Trig
-#define ECHO 2    // Arduino Pin --> HC-SR04 Echo
+#define TRIGGER_PIN 3 // Arduino Pin --> HC-SR04 Trig
+#define ECHO_PIN 2    // Arduino Pin --> HC-SR04 Echo
 #define PULSE_TO_CM_DIVIDER 58 // from HC-SR04 User's Manual v1.0
 
-#define NUM_LEDS 1
-#define DATA_PIN 9
-#define BUZZER 6
+#define NUM_LEDS 24
+#define LED_PIN 9
+#define BUZZER_PIN 6
 
 #define MEASURE_INTERVAL 20 // milliseconds a measurement should happen, max. HC-SR04 is 50 per second
-#define MIN_DISTANCE_CM 100
+#define MAX_DISTANCE_CM 34
+
+#define FLY_THROUGH_GRACE_PERIOD 1000
 
 #define DEBUG 0
 
@@ -19,44 +21,79 @@
 CRGB leds[NUM_LEDS];
 
 unsigned long last_measured = 0;
-unsigned long last_LED_anim = 0;
+unsigned long flyThroughAt  = 0;
 unsigned long time = 0;
+bool flyThroughDetected = false;
 int last_distance = 0;
-bool signal_LED = false;
 
 void setup()
 {
-  pinMode(TRIGGER, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(TRIGGER, HIGH); //Signal abschalten
-  digitalWrite(TRIGGER, HIGH);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, HIGH); //Signal abschalten
+  digitalWrite(TRIGGER_PIN, HIGH);
 
   Serial.begin(57600);
 
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   time = millis();
 }
 
 int getDistanceInCentimeter()
 {
-  digitalWrite(TRIGGER, LOW);
+  digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(3);
   noInterrupts();
-  digitalWrite(TRIGGER, HIGH);
+  digitalWrite(TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER, LOW);
-  long duration = pulseIn(ECHO, HIGH);
+  digitalWrite(TRIGGER_PIN, LOW);
+  long duration = pulseIn(ECHO_PIN, HIGH);
   interrupts();
 
   long distance = duration / PULSE_TO_CM_DIVIDER;
   return (distance);
 }
 
-unsigned long rounding_filter(unsigned long l)
+void makeNoise()
 {
-  if (l % 10 > 5) return (l / 10) * 10 + 1;
-  return (l / 10) * 10;
+  int delta = (time - flyThroughAt );
+  if (delta > 500)
+  {
+    noTone(BUZZER_PIN);
+  }
+  else if (delta > 0)
+  {
+    tone(BUZZER_PIN, 2000 + abs(map(delta, 0, 500, -1000, 1000)));
+  }
+}
+
+void animateLed()
+{
+  int delta = (time - flyThroughAt);
+  if (delta > 500)
+  {
+    fill_solid(leds, NUM_LEDS, CHSV( 190, 255, 125));
+    FastLED.show();
+  }
+  else if (delta > 400)
+  {
+    int value = abs(map(delta, 400, 500, 0, 125));
+    fill_solid(leds, NUM_LEDS, CHSV( 190, 255, value));
+    FastLED.show();
+  }
+  else if (delta > 100)
+  {
+    int value = 255 - abs(map(delta, 100, 400, -255, 255));
+    fill_solid(leds, NUM_LEDS, CHSV( 0, 255, value));
+    FastLED.show();
+  }
+  else if (delta > 0)
+  {
+    int value = abs(map(delta, 0, 100, 125, 0));
+    fill_solid(leds, NUM_LEDS, CHSV( 190, 255, value));
+    FastLED.show();
+  }
 }
 
 void loop()
@@ -74,34 +111,27 @@ void loop()
       Serial.write(" cm\n");
     }
 
-    distance = rounding_filter(distance);
-    if (distance != last_distance && distance < MIN_DISTANCE_CM)
+    if (distance != last_distance && distance < MAX_DISTANCE_CM)
     {
-      last_distance = distance;
-      if (!signal_LED)
+      if (!flyThroughDetected)
       {
-        last_LED_anim = time;
+        flyThroughAt  = time;
       }
-      signal_LED = true;
+      flyThroughDetected = true;
     }
   }
 
-  if (signal_LED)
+  if (flyThroughDetected)
   {
-    int delta = (time - last_LED_anim);
-    if (delta > 2000)
+    int delta = (time - flyThroughAt);
+    if (delta > FLY_THROUGH_GRACE_PERIOD)
     {
-      signal_LED = false;
-      leds[0] = CRGB::Black;
-      noTone(BUZZER);
+      flyThroughDetected = false;
     }
-    else if (delta > 0)
-    {
-      leds[0].r = 255 - abs(map(delta, 0, 2000, -255, 255));
-      tone(BUZZER, 2000 + abs(map(delta, 0, 2000, -1000, 1000)));
-    }
-    FastLED.show();
   }
+
+  animateLed();
+  makeNoise();
 }
 
 
